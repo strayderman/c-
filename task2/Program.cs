d28b76718b6ae3e0d2beeb33c947f34fa635e79e
Vitali Basko <strayderman@mail.ru>
2023-02-12 16:08:10 +0300

    Task 2 Решено

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру: ");
+int a = Convert.ToInt32(Console.ReadLine()); //int.Parse(Console.ReadLine());
+Console.WriteLine("Введите цифру: ");
+int b = Convert.ToInt32(Console.ReadLine()); //int.Parse(Console.ReadLine());
+if (a > b)
+ {Console.WriteLine("Большее число: ");
+ Console.WriteLine(a);
+ Console.WriteLine("Меньшее число: ");
+ Console.WriteLine(b);} 
+ if(a < b)
+ {Console.WriteLine("Большее число: ");
+ Console.WriteLine(b);
+ Console.WriteLine("Меньшее число: ");
+ Console.WriteLine(a);}
