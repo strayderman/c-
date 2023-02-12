27674b045247068ecab0ba70b5b0c380318922ba
Vitali Basko <strayderman@mail.ru>
2023-02-12 18:06:56 +0300

    task4 добавлено условие

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите цифру: ");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите цифру: ");
 int a = Convert.ToInt32(Console.ReadLine()); 
 Console.WriteLine("Введите цифру: ");
 int b = Convert.ToInt32(Console.ReadLine()); 
