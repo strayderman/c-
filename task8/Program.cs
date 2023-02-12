59bcb5c319eb60acd585a4ee9ca94b2abb145f4a
Vitali Basko <strayderman@mail.ru>
2023-02-12 17:55:15 +0300

    C# in task8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Четные числа" )
+for(int i = 1; i <= N; i++) 
+{
+   if(i % 2 == 0){
+    //Console.WriteLine("Четные числа: " );
+    Console.Write(i );}
+}
