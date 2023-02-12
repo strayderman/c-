5a3cffa3b938d60c30cc04f9954a3efe7180c5c4
Vitali Basko <strayderman@mail.ru>
2023-02-12 17:57:51 +0300

    task8 решено

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Введите цифру: ");
 int N = Convert.ToInt32(Console.ReadLine());
-Console.Write("Четные числа" )
+Console.Write("Четные числа:" );
 for(int i = 1; i <= N; i++) 
 {
    if(i % 2 == 0){
     //Console.WriteLine("Четные числа: " );
-    Console.Write(i );}
+    Console.Write(i);}
 }
