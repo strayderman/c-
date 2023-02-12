dbce7f8abd08d69ecbee99cc8aa97ae9c6480426
Vitali Basko <strayderman@mail.ru>
2023-02-12 16:46:19 +0300

    task6 решено

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 0)
+{Console.WriteLine("Четное ");}
+if(a % 2 != 0)
+{Console.WriteLine("Нечетное ");}
