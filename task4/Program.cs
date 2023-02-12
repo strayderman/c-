e67f2a0bedac3da3f5aa319ee5bfb6bebab76910
Vitali Basko <strayderman@mail.ru>
2023-02-12 16:27:33 +0300

    task4 сделано

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру: ");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите цифру: ");
+int b = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите цифру: ");
+int c = Convert.ToInt32(Console.ReadLine()); 
+int max = a;
+if(b > max) 
+{max = b;}
+if(c > max) 
+{max = c;}
+Console.WriteLine("Максимальное число: ");
+Console.WriteLine(max);
