3b920dcd259fe86cde587b707b9fb2e5228a3d0a
Vitali Basko <strayderman@mail.ru>
2023-02-12 18:05:22 +0300

    task2 добавлено условие

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.WriteLine("Введите цифру: ");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите цифру: ");
 int a = Convert.ToInt32(Console.ReadLine()); //int.Parse(Console.ReadLine());
 Console.WriteLine("Введите цифру: ");
 int b = Convert.ToInt32(Console.ReadLine()); //int.Parse(Console.ReadLine());
