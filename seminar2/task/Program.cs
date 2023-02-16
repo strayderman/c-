364b3037716ae7ffb64086032866fd2aa6dbfac6
Vitali Basko <strayderman@mail.ru>
2023-02-16 23:27:44 +0300

    task10 13 15 good

--- seminar2/task/Program.cs
+++ seminar2/task/Program.cs
@@ -4,19 +4,85 @@
 // то программа должна выводить слово «FizzBuzz».
 // (доп задача по желанию, разберем на семинаре)
 
-//string a = Console.WriteLine("Fizz")
-int[] a = new int[16]; 
-int q = a.Length;
-for (int i = 1; i < q; i++)// Заполнили массив от 1 до 15
-{
-    a[i] = i + 1;
-    //Console.WriteLine(i);
-    if(a[i] % 3 == 0 && a[i] % 5 == 0)
-    {
+
+// int[] a = new int[16]; 
+// int q = a.Length;
+// // string F = "Fizz,";
+// // string B = "Buzz,";
+// // string FB = "FizzBuzz,";
+// for (int i = 1; i < q; i++)// Заполнили массив 
+// {
+//     a[i] = i + 1;
         
-        string b = "FizzBuss";
-        int b1 = Convert.ToInt32(b);
-        a[i] = b1;
-        Console.WriteLine(i);
-    }
-}
+//     if(a[i] % 3 == 1)    // Проверяем кратность 3)
+//     { 
+//         Console.Write("Fizz,");
+//     }
+//     if(a[i] % 5 == 1)    // Проверяем кратность 5)
+//      {
+//         Console.Write("Buzz,");
+//      }
+//      if (a[i] % 3 == 1 && a[i] % 5 == 1) // Проверяем кратность и 3 и 5)
+//      {
+//         Console.Write("FizzBuzz,");
+//      }
+//      else
+//         {
+//             Console.Write(i + ",");
+//         }
+   
+// }
+
+// int[] a = new int[16]; 
+// int q = a.Length;
+// for (int i = 0; i < 15; i++)// Заполнили массив 
+// {
+//     int a = i++;
+//     if(a % 3 == 0)    // Проверяем кратность 3)
+//     { 
+//         Console.Write("Fizz,");
+//     }
+//        if(a % 5 == 0)    // Проверяем кратность 5)
+//      {
+//         Console.Write("Buzz,");
+//      }
+//           if (a % 3 == 0 && a % 5 == 0) // Проверяем кратность и 3 и 5)
+//      {
+//         Console.Write("FizzBuzz,");
+//      }
+//      else
+//         {
+//             Console.Write(a + ",");
+//         }
+// }
+
+
+// string F = "Fizz,";
+// string B = "Buzz,";
+// string FB = "FizzBuzz,";
+
+
+// for (int i = 0; i < 15; i++)// Заполнили массив 
+// {
+//     int a = i++;
+//    if (a % 3 == 0 && a % 5 == 0) // Проверяем кратность и 3 и 5)
+//         {
+//         Console.Write("FizzBuzz,");
+//         }
+//     if(a % 3 == 0)    // Проверяем кратность 3)
+//     { 
+//         Console.Write("Fizz,");
+//     }
+//          if(a % 5 == 0)    // Проверяем кратность 5)
+//         {
+//         Console.Write("Buzz,");
+//         }
+          
+//      else
+//         {
+//             Console.Write(a + ",");
+//         }
+// }
+int a = 6;
+int b = 3;
+Console.Write (a % 3 );
