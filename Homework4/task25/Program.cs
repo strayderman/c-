b2ec3b4668738c4c1398f4bda097693d01f09571
Vitali Basko <strayderman@mail.ru>
2023-02-22 21:55:56 +0300

    add Homework4/task 25 done

--- Homework4/task25/Program.cs
+++ Homework4/task25/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double aPowerb(double a, double b)
+{
+    double c = Math.Pow(a,b);
+    return(c);
+}
+
+Console.WriteLine("Введите 1-е число: ");
+double a1 = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-е число: ");
+double b1 = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Результат возведения 1-го числа в степень 2-го = " + aPowerb(a1, b1));
--- Homework4/task27/Program.cs
+++ Homework4/task27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Sum(namber)
+{
+    while (num > 0)
+    {
+        int z =                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     num / 10
+    }
+
+}
