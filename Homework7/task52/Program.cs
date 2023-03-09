0ec0edac71cc9203977b6f6276c5e1bbbb36f00a
Vitali Basko <strayderman@mail.ru>
2023-03-09 22:27:06 +0300

    task52 not yet

--- Homework7/task52/Program.cs
+++ Homework7/task52/Program.cs
@@ -5,3 +5,74 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+string UserMessage(string message)
+{
+    return $"Введите количество {message} >>> ";
+}
+
+
+int GetRows()
+{
+    Console.Write($"{UserMessage("строк")}");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    return rows;
+}
+
+int GetCols()
+{
+    Console.Write($"{UserMessage("столбцов")}");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    return cols;
+}
+// метод заполнения массива
+int[,] FillMatrix(int[,] matr)
+{
+    int[,] newMatrix = matr;
+    for (int rows = 0; rows < matr.GetLength(0); rows++)
+    {
+        for (int cols = 0; cols < matr.GetLength(1); cols++)
+        {
+            newMatrix[rows, cols] = new Random().Next(0, 50);
+        }
+    }
+    return newMatrix;
+}
+void PrintMatrix(int[,] mat)
+{
+    for (int rows = 0; rows < mat.GetLength(0); rows++)
+    {
+        for (int cols = 0; cols < mat.GetLength(1); cols++)
+        {
+            Console.Write($"{mat[rows, cols]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] SumArray(int[,] matrixx)
+{
+    double[] result = new double[matrixx.GetLength(1)];
+    for (int j = 0; j < matrixx.GetLength(1); j++)
+    {
+        double Sum = 0;
+        for (int i = 0; i < matrixx.GetLength(0); i++)
+        {
+            Sum += matrixx[i, j];
+        }
+        result[j] = Math.Round(Sum / matrixx.GetLength(0), 2);
+    }
+    return result;
+}
+void Task52()
+{
+    int rows = GetRows();
+    int cols = GetCols();
+    int[,] matrix = new int[rows, cols];
+    FillMatrix(matrix);
+    PrintMatrix(matrix);
+    SumArray(matrix);
+    Console.WriteLine();
+    Console.Write($" Среднее арифметическое каждого столбца = {SumArray(matrix)}");
+}
+Task52();
