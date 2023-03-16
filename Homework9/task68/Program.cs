c5a0afd55c1f4789227795c1c67011419ec4678a
Vitali Basko <strayderman@mail.ru>
2023-03-16 15:31:25 +0300

    HW9 is done

--- Homework9/task68/Program.cs
+++ Homework9/task68/Program.cs
@@ -12,12 +12,12 @@ int result = Akkerman(x, y);
 Console.WriteLine(result);
 int Akkerman(int m, int n)
 {
-    if (m < 0 || n < 0)
-        return -1;
-    else if (m == 0)
+        if (m == 0)
         return n + 1;
     else if (m > 0 && n == 0)
         return Akkerman(m - 1, 1);
     else if (m > 0 && n > 0)
         return Akkerman(m - 1, Akkerman(m, n - 1));
+    else return n + 1;
+    
 }
